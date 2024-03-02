5dcc59535c14300a14f660a9da50ff665a22d337
shivam patel <shivampatel6514@gmail.com>
2024-03-03 02:43:09 +0530

    code push

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,21 @@
 from django.urls import path
-from .views import themeList,userList,contactList,contactListId
+from .views import themeList,userList,contactList,contactListId,userListId,themeListId,sitedataList,sitedataListId
 
 urlpatterns = [
     path('themes/', themeList, name='themeList'),
-    path('users/', userList, name='themeList'),
+    path('themes/<str:id>/', themeListId, name='themeListId'),
+
+    path('users/', userList, name='userList'),
+    path('users/<str:id>/', userListId, name='userListId'),
+
     path('contacts/', contactList, name='contactList'),
     path('contacts/<str:id>/', contactListId, name='contactListId'),
-    # path('create_contact/', createContact, name='createContact'),
+
+
+    path('sitedata/', sitedataList, name='sitedataList'),
+    path('sitedata/<str:id>/', sitedataListId, name='sitedataListId'),
+    
+    
 
 
 ]
--- api/views.py
+++ api/views.py
@@ -6,10 +6,11 @@ from django.http import HttpResponse,Http404
 from django.views.decorators.csrf import csrf_exempt
 import json
 from datetime import datetime
+from passlib.hash import bcrypt
 
 region = 'us-west-2'
-aws_access_key_id = 'AKIAQFC4UVNMBVG3QHZEjhvjh'
-aws_secret_access_key = 'lHRXhrf4q+Nb+cx0aaBz9W+JLdR2fAdjmo,knjkjkjnEKwUxS'
+aws_access_key_id = 'AKIAQFC4UVNMBVG3QHZE---jhbhjbhvu'
+aws_secret_access_key = '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----utyftufu'
     
 # Create a DynamoDB client 
 dynamodb = boto3.resource('dynamodb', region_name=region, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
@@ -22,14 +23,110 @@ table = dynamodb.Table(table_name)
 # async def get_theme():
 #     return get_data_from_table("themes")
 
-
+@csrf_exempt 
 def themeList(request):
+    if request.method == 'POST':
+        data = request.POST  # Access POST data
+        data = json.loads(request.body.decode('utf-8'))
+        # print(data.get('name'))
+
+        table_name = 'themes'
+        table = dynamodb.Table(table_name)
+        id = str(uuid.uuid4())
+        created_at = str(datetime.now())
+        # hashed_password = bcrypt.hash(data.get('password'))
+
+        # Insert contacts into DynamoDB
+        with table.batch_writer() as batch:
+            batch.put_item(Item={
+                'id': id,
+                'name': data.get('name'),
+                'demo': data.get('demo'),
+                'keys': data.get('keys'),
+                'client_keys': data.get('client_keys'),
+                'created_at': created_at
+            })
+        data = findById("themes",id)    
+        return JsonResponse({"message": "Data saved successfully", "status": True, "data": data})
+
     data =  get_data_from_table("themes")
     return JsonResponse(data)
 
+@csrf_exempt
+def themeListId(request,id):
+    idChecks = idCheck("themes",id)
+    if idChecks is None:
+        return JsonResponse({"message": "Data not found", "status": False, "data": []})
+    
+    if request.method == 'GET':
+        data = get_data_from_table_by_id("themes",id)
+        return JsonResponse(data)
+    
+    elif request.method == 'DELETE':
+        print("dele data")
+        data = delete_item_from_table("themes", id)
+        if data["status"]:
+            return JsonResponse(data)   
+    
+    elif request.method == 'POST':
+        data = request.POST  # Access POST data
+        return JsonResponse({'message': 'This is a POST request.', 'data': dict(data)})
+
+
+
+@csrf_exempt 
 def userList(request):
+    if request.method == 'POST':
+        data = request.POST  # Access POST data
+        data = json.loads(request.body.decode('utf-8'))
+        print(data.get('name'))
+
+        table_name = 'users'
+        table = dynamodb.Table(table_name)
+        id = str(uuid.uuid4())
+        created_at = str(datetime.now())
+        hashed_password = bcrypt.hash(data.get('password'))
+
+        # Insert contacts into DynamoDB
+        with table.batch_writer() as batch:
+            batch.put_item(Item={
+                'id': id,
+                'name': data.get('name'),
+                'email': data.get('email'),
+                'password': hashed_password,
+                'domain': data.get('domain'),
+                'mobile': data.get('mobile'),
+                'role_type': data.get('massage'),
+                'created_at': created_at
+            })
+        data = findById("users",id)    
+        return JsonResponse({"message": "Data saved successfully", "status": True, "data": data})
+
     data =  get_data_from_table("users")
     return JsonResponse(data)
+
+@csrf_exempt 
+def userListId(request,id):
+    print(request.method)
+    idChecks = idCheck("users",id)
+    if idChecks is None:
+        return JsonResponse({"message": "Data not found", "status": False, "data": []})
+    
+    if request.method == 'GET':
+        data = get_data_from_table_by_id("users",id)
+        return JsonResponse(data)
+    
+    elif request.method == 'DELETE':
+        print("dele data")
+        data = delete_item_from_table("users", id)
+        if data["status"]:
+            return JsonResponse(data)   
+    
+    elif request.method == 'POST':
+        data = request.POST  # Access POST data
+        return JsonResponse({'message': 'This is a POST request.', 'data': dict(data)})
+
+
 
 @csrf_exempt 
 def contactList(request):
@@ -80,14 +177,58 @@ def contactListId(request,id):
         data = request.POST  # Access POST data
         return JsonResponse({'message': 'This is a POST request.', 'data': dict(data)})
 
-# @csrf_exempt 
-# def createContact(request):
-#     return 
 
-# def themeList(request):
-#     data =  get_data_from_table("themes")
 
-#     return JsonResponse(data)
+# sitedataList,sitedataListId
+@csrf_exempt 
+def sitedataList(request):
+    if request.method == 'POST':
+        data = request.POST  # Access POST data
+        data = json.loads(request.body.decode('utf-8'))
+        print(data.get('name'))
+
+        table_name = 'site_data'
+        table = dynamodb.Table(table_name)
+        id = str(uuid.uuid4())
+        created_at = str(datetime.now())
+
+        with table.batch_writer() as batch:
+            batch.put_item(Item={
+                'id': id,
+                'name': data.get('name'),
+                'description': data.get('description'),
+                'domain': data.get('domain'),
+                'client_keys': data.get('client_keys'),
+                'keys': data.get('keys'),
+                'theme_id': data.get('theme_id'),
+                'user_id': data.get('user_id'),
+                'created_at': created_at
+            })
+        data = findById("site_data",id)    
+        return JsonResponse({"message": "Data saved successfully", "status": True, "data": data})
+
+    data =  get_data_from_table("site_data")
+    return JsonResponse(data)
+
+@csrf_exempt 
+def sitedataListId(request,id):
+    idChecks = idCheck("site_data",id)
+    if idChecks is None:
+        return JsonResponse({"message": "Data not found", "status": False, "data": []})
+    
+    if request.method == 'GET':
+        data = get_data_from_table_by_id("site_data",id)
+        return JsonResponse(data)
+    
+    elif request.method == 'DELETE':
+        data = delete_item_from_table("site_data", id)
+        if data["status"]:
+            return JsonResponse(data)   
+    
+    elif request.method == 'POST':
+        data = request.POST  # Access POST data
+        return JsonResponse({'message': 'This is a POST request.', 'data': dict(data)})
+
 
 
 
